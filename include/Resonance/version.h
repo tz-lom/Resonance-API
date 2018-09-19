b638d2ed1d98462e456da9d3fa4810de535ed322
Nuzhdin Yury <nuzhin.urii@gmail.com>
2018-09-19 23:39:33 +0200

    Set new version

--- include/Resonance/version.h
+++ include/Resonance/version.h
@@ -1,4 +1,4 @@
 #pragma once
 #define RESONANCE_BUILD_DATE "2018-09-19"
-#define RESONANCE_BUILD_REVISION "3.3-r1-101-g22dd20e-dirty"
+#define RESONANCE_BUILD_REVISION "3.4"
 #define RESONANCE_BUILD_BRANCH "develop"
